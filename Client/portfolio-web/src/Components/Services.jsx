a98dee98742c8933092492e9d4807d56ecf65bdb
Mamunfx <md01993049420@gmail.com>
2025-04-05 22:08:54 +0600

    last commit of today

--- Client/portfolio-web/src/Components/Services.jsx
+++ Client/portfolio-web/src/Components/Services.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { GiPencilBrush } from "react-icons/gi";
 const Services = () => {
   return (
     <div>
@@ -7,18 +7,15 @@ const Services = () => {
 
       <div className="grid grid-cols-3 w-11/12 mx-auto space-y-4">
 
-        <div className="card w-96 ">
-          <figure className="px-10 pt-10">
-            <img
-              src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-              alt="Shoes"
-              className="rounded-xl"
-            />
-          </figure>
-          <div className="card-body items-left text-left">
-            <h2 className="card-title">Web Development</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
-            <div className="card-actions">
+        <div className="card text-left">
+  
+          <div className=" items-left text-left  space-y-6">
+          <div className="text-7xl  text-green-700">
+           <GiPencilBrush />
+           </div>
+            <h2 className=" font-semibold text-3xl">Web Development</h2>
+            <p className="font-semibold">Duis molestie enim mattis gravida viverra. Fusce ut eros augue. Sed id mauris vel neque</p>
+            <div className="">
               <button className="btn btn-primary">Read More</button>
             </div>
           </div>
