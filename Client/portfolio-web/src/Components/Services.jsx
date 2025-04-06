69e19c365b18af3b688a65b6df4e2ce97bf8069e
Mamunfx <md01993049420@gmail.com>
2025-04-06 08:09:52 +0600

    first commit of the day

--- Client/portfolio-web/src/Components/Services.jsx
+++ Client/portfolio-web/src/Components/Services.jsx
@@ -1,63 +1,101 @@
 import React from "react";
 import { GiPencilBrush } from "react-icons/gi";
+import { FaCameraRetro } from "react-icons/fa";
+import { RiVideoOnAiLine } from "react-icons/ri";
+import { FaPenFancy } from "react-icons/fa";
+import { FaLaptopCode } from "react-icons/fa";
+import { FaMobileRetro } from "react-icons/fa6";
 const Services = () => {
   return (
     <div>
       <h1 className="text-4xl font-bold my-8 mt-20 text-center">My Services</h1>
 
-      <div className="grid grid-cols-3 w-11/12 mx-auto space-y-4">
 
-        <div className="card text-left">
-  
-          <div className=" items-left text-left  space-y-6">
-          <div className="text-7xl  text-green-700">
-           <GiPencilBrush />
-           </div>
-            <h2 className=" font-semibold text-3xl">Web Development</h2>
-            <p className="font-semibold">Duis molestie enim mattis gravida viverra. Fusce ut eros augue. Sed id mauris vel neque</p>
-            <div className="">
-              <button className="btn btn-primary">Read More</button>
-            </div>
-          </div>
-        </div>
+     <div className="grid grid-cols-3 w-11/12 mx-auto mt-24 gap-4 ">
 
-        <div className="card w-96 ">
-          <figure className="px-10 pt-10">
-            <img
-              src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-              alt="Shoes"
-              className="rounded-xl"
-            />
-          </figure>
-          <div className="card-body items-left text-left">
-            <h2 className="card-title">Coding</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
-            <div className="card-actions">
-              <button className="btn btn-primary">Read More</button>
-            </div>
-          </div>
-        </div>
+<div className="card text-left">
+  <div className=" items-left text-left  space-y-6">
+  <div className="text-7xl  text-green-700">
+   <GiPencilBrush />
+   </div>
+    <h2 className=" font-semibold text-3xl">Web Development</h2>
+    <p className="font-semibold">Duis molestie enim mattis gravida viverra. Fusce ut eros augue. Sed id mauris vel neque</p>
+    <div className="">
+    <button className="btn  text-white  rounded-full bg-green-700">Read More</button>
+    </div>
+  </div>
+</div>
 
-        <div className="card w-96 ">
-          <figure className="px-10 pt-10">
-            <img
-              src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-              alt="Shoes"
-              className="rounded-xl"
-            />
-          </figure>
-          <div className="card-body items-left text-left">
-            <h2 className="card-title">App Development</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
-            <div className="card-actions">
-              <button className="btn btn-primary">Read More</button>
-            </div>
-          </div>
-        </div>
+<div className="card text-left">
+  <div className=" items-left text-left  space-y-6">
+  <div className="text-7xl  text-green-700">
+  <FaLaptopCode />
+   </div>
+    <h2 className=" font-semibold text-3xl">Coding</h2>
+    <p className="font-semibold">Duis molestie enim mattis gravida viverra. Fusce ut eros augue. Sed id mauris vel neque</p>
+    <div className="">
+    <button className="btn  text-white  rounded-full bg-green-700">Read More</button>
+    </div>
+  </div>
+</div>
 
-       
+<div className="card text-left">
+  <div className=" items-left text-left  space-y-6">
+  <div className="text-7xl  text-green-700">
+  <FaMobileRetro />
+   </div>
+    <h2 className=" font-semibold text-3xl">App Development</h2>
+    <p className="font-semibold">Duis molestie enim mattis gravida viverra. Fusce ut eros augue. Sed id mauris vel neque</p>
+    <div className="">
+    <button className="btn  text-white  rounded-full bg-green-700">Read More</button>
+    </div>
+  </div>
+</div>
 
-      </div>
+</div>
+
+<div className="grid grid-cols-3 w-11/12 mx-auto mt-24 gap-4 ">
+<div className="card text-left">
+  <div className=" items-left text-left  space-y-6">
+  <div className="text-7xl  text-green-700">
+  <FaCameraRetro />
+   </div>
+    <h2 className=" font-semibold text-3xl">Photography</h2>
+    <p className="font-semibold">Duis molestie enim mattis gravida viverra. Fusce ut eros augue. Sed id mauris vel neque</p>
+    <div className="">
+    <button className="btn  text-white  rounded-full bg-green-700">Read More</button>
+    </div>
+  </div>
+</div>
+
+<div className="card text-left">
+  <div className=" items-left text-left  space-y-6">
+  <div className="text-7xl  text-green-700">
+  <RiVideoOnAiLine />
+   </div>
+    <h2 className=" font-semibold text-3xl">Video Editing</h2>
+    <p className="font-semibold">Duis molestie enim mattis gravida viverra. Fusce ut eros augue. Sed id mauris vel neque</p>
+    <div className="">
+    <button className="btn  text-white  rounded-full bg-green-700">Read More</button>
+    </div>
+  </div>
+</div>
+
+<div className="card text-left">
+  <div className=" items-left text-left  space-y-6">
+  <div className="text-7xl  text-green-700">
+  <FaPenFancy />
+   </div>
+    <h2 className=" font-semibold text-3xl">Copy Writing</h2>
+    <p className="font-semibold">Duis molestie enim mattis gravida viverra. Fusce ut eros augue. Sed id mauris vel neque</p>
+    <div className="">
+    <button className="btn  text-white  rounded-full bg-green-700">Read More</button>
+    </div>
+  </div>
+</div>
+</div>
+
+
     </div>
   );
 };
