04c6bcb94716923460c937b20541c3c2d582e4db
Mamunfx <md01993049420@gmail.com>
2025-04-08 08:40:12 +0600

    old push

--- Client/portfolio-web/src/Components/Navbar.jsx
+++ Client/portfolio-web/src/Components/Navbar.jsx
@@ -8,9 +8,9 @@ const Navbar = () => {
     return (
         <div>
             <div className="navbar bg-base-100 fixed top-0 left-0 w-full z-50">
-                <div className="navbar-end">
-                    <div className="dropdown">
-                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                <div className="navbar-end w-full ">
+                    <div className="dropdown relative">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden ">
                             <svg
                                 xmlns="http://www.w3.org/2000/svg"
                                 className="h-5 w-5"
@@ -26,7 +26,7 @@ const Navbar = () => {
                         </div>
                         <ul
                             tabIndex={0}
-                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow absolute right-[1rem]">
                             <li>
                                 <a
                                     href="#banner"
